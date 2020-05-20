390e3e840abd7147b32739e7c678f2e6c92f97e4
yordi0406 <65675837+yordi0406@users.noreply.github.com>
2020-05-20 15:33:58 -0300

    Add files via upload

---
+++ Letras repetidas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Problema_Mutante
+{
+    class Letras_repetidas
+    {
+        public int  ConteoLetras(string[] datos)
+        {            
+            string[] lista = new string[datos.Length];
+           
+            int valormutante = 0;
+
+            for (int i = 0; i < datos.Length; i++)
+            {
+                int contador = 0;
+
+
+                for (int j = 0; j < datos.Length; j++)
+                {
+                    if (datos[i] == datos[j] )
+                    {
+                        contador++;
+
+
+                        if (numero(datos[i]))
+                        {
+                            lista[i] = datos[i];
+                        }
+                    }
+                }
+                if (lista[i] != " " ^ contador <4)
+                {
+                    valormutante = 1;
+                   
+                }
+                
+            }
+        
+            bool numero(string num)
+            {
+                for (int i = 0; i < lista.Length; i++)
+                {
+                    if (lista[i] == num)
+                    {
+                        return false;
+                    }
+
+                }
+                return true;
+            }
+
+            return valormutante;
+
+        }
+
+    }
+}
---
+++ MatrizVH.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problema_Mutante
+{
+    class MatrizVH:Letras_repetidas
+    {
+        public void CalculoMatriz()
+        {
+
+            string[] fila = new string[] { };
+
+            string[,] matrizPrincipal = new string[,] { };
+
+            
+            for (int i = 0; i < matrizPrincipal.GetLength(0); i++)
+            {
+
+                int contador = 0;
+                contador = contador + i;
+                fila[i] = matrizPrincipal[i, 0];
+
+                Console.WriteLine(" el resultado horizontal es " + fila[i]);
+
+            }
+            for (int i = 0; i < matrizPrincipal.GetLength(0); i++)
+            {
+                fila[i] = matrizPrincipal[i, i];
+                Console.WriteLine(" el resultado matriz diagonal  es " + fila[i]);
+            }
+            for (int j = 0; j < matrizPrincipal.GetLength(1); j++)
+            {
+                fila[j] = matrizPrincipal[1, j];
+                Console.WriteLine(" el resultado vertical es " + fila[j]);
+            }
+        }
+    }
+}
+
+
+
+
+
+    
+    
